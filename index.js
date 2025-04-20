d3d57550741e0badaff044e7b0f7dc4f7da85663
YahyaAyyashRabbani <142917111+YahyaAyyashRabbani@users.noreply.github.com>
2025-04-20 15:24:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
-app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.send("Welcome to the homepage!"));
 app.use(UserRoute);
 
 app.listen(5000, () => console.log("Server connected"));
